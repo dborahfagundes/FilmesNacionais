b7e1479b3285851d42d4501597507e748caad83e
Déborah <deborahfagund@gmail.com>
2024-09-23 15:57:26 -0300

    Update

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import filmesRoutes from './routes/filmesRoutes.js';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/api/filmes', filmesRoutes);
